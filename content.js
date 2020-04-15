e657d511dd9a2367abf13325d6d7a5ea2d75f6d1
spanasyuk <sergiy.panasyuk88@gmail.com>
2020-04-16 02:36:56 +0300

    ver 1.0 is ready: actual currency rates, fuel type, refactoring

--- content.js
+++ content.js
@@ -6,6 +6,17 @@ function getIntValue(source, element) {
 	return parseInt(source.split(element)[1].match(/\d+/)[0]);
 }
 
+function getStringValue(source, element) {
+	return String(source.split(element)[2].match(/[a-z]+/)[0]);
+}
+
+function getCurrencyRate(currency1, currency2) {
+	var http = new XMLHttpRequest()
+	http.open("GET", "https://api.exchangeratesapi.io/latest?base="+currency1+"&symbols="+currency2, false)
+	http.send()
+	return JSON.parse(http.responseText).rates[currency2]
+}
+
 //GET VALUE PARSING HTML
 //var price_string_pln = getElementByXpath("//*[@class='offer-price__number']/text()");
 //var price_pln = parseInt(String(price_string_pln.textContent).replace(/ /g, ''), 10);
@@ -16,33 +27,46 @@ var json_str = getElementByXpath("//*[contains(text(),'GPT.targeting')]").textCo
 const price_pln = getIntValue(json_str, "price_raw");
 const year = getIntValue(json_str, "year");
 const capacity = getIntValue(json_str, "capacity");
+const fuel_type = getStringValue(json_str, "fuel_type");
 
 // FORMULA
+const customPercent = 0.055
+const currentYear = new Date().getFullYear()
+const exciseFuelKoef = (fuel_type == 'diesel') ? 75 : 50
+const maxYearKoef = 15
+const taxPercent = 0.2
+
 function getYearKoef(year){
-	if ((2020-year) > 15) {
-		return 15;
+	if ((currentYear-year) > maxYearKoef) {
+		return maxYearKoef;
 	} else {
-		return 2020-year;
+		return currentYear-year;
 	}
 }
 
 function getCustomValue(price){
-	return price*0.055;
+	return price*customPercent;
 }
 
 function getExciseValue(capacity, year){
-	return 50 * (capacity/1000) * getYearKoef(year)
+	return exciseFuelKoef * (capacity/1000) * getYearKoef(year)
 }
 
 function getTaxValue(price){
-	return price*0.2;
+	return price*taxPercent;
 }
 
-const price_eur = price_pln / 4.55;
+var price_eur = price_pln * getCurrencyRate("PLN","EUR");
 
-var total_price_eur_without_tax = price_eur + getCustomValue(price_eur) + getExciseValue(capacity, year);
-var total_price_eur = total_price_eur_without_tax + getTaxValue(total_price_eur_without_tax);
+var total_price_eur_without_tax = price_eur 
+								+ getCustomValue(price_eur) 
+								+ getExciseValue(capacity, year);
+var total_price_eur = total_price_eur_without_tax 
+					+ getTaxValue(total_price_eur_without_tax);
 console.log(total_price_eur)
+
+var total_price_usd = total_price_eur * getCurrencyRate("EUR","USD");
+console.log(total_price_usd)
 
 chrome.runtime.sendMessage({
   from: 'content',
@@ -53,6 +77,7 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
   if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
 	var domInfo = {
       total_price_eur: Math.round(total_price_eur),
+	  total_price_usd: Math.round(total_price_usd),
       initial_price_eur: Math.round(price_eur),
       custom_value: Math.round(getCustomValue(price_eur)),
 	  excise_value: Math.round(getExciseValue(capacity, year)),
