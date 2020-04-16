76e6056adb70cf88e8832988c0c53f0146be55a6
spanasyuk <sergiy.panasyuk88@gmail.com>
2020-04-17 02:10:31 +0300

    added a support of hybrids, small refactoring of calculations

--- content.js
+++ content.js
@@ -25,12 +25,14 @@ const price_pln = getIntValue(json_str, "price_raw");
 const year = getIntValue(json_str, "year");
 const capacity = getIntValue(json_str, "capacity");
 const fuel_type = getStringValue(json_str, "fuel_type");
+console.log(fuel_type);
 
 // Tax calculation
-const customPercent = 0.055;
+const customPercent = (fuel_type == "hybrid") ? 0.1 : 0.055;
 const currentYear = new Date().getFullYear();
 const maxYearKoef = 15;
 const taxPercent = 0.2;
+const hybrid_excise = 100;
 
 function getExciseBaseRate(type, capacity){
 	if (type == "petrol"){
@@ -54,8 +56,8 @@ function getCustomValue(price){
 	return price*customPercent;
 }
 
-function getExciseValue(capacity, year){
-	return getExciseBaseRate(fuel_type,capacity) * (capacity/1000) * getYearKoef(year);
+function getExciseValue(capacity, year, fuel_type){
+	return (fuel_type == 'hybrid') ? hybrid_excise : (getExciseBaseRate(fuel_type,capacity) * (capacity/1000) * getYearKoef(year));
 }
 
 function getTaxValue(price){
@@ -63,10 +65,11 @@ function getTaxValue(price){
 }
 
 var price_eur = price_pln * getCurrencyRate("PLN","EUR");
-
-var total_price_eur_without_tax = price_eur + getCustomValue(price_eur) + getExciseValue(capacity, year);
-var total_price_eur = total_price_eur_without_tax + getTaxValue(total_price_eur_without_tax);
-
+var custom_value = getCustomValue(price_eur);
+var excise_value = getExciseValue(capacity, year, fuel_type);
+var total_price_eur_without_tax = price_eur + custom_value + excise_value;
+var tax_value = getTaxValue(total_price_eur_without_tax);
+var total_price_eur = total_price_eur_without_tax + tax_value;
 var total_price_usd = total_price_eur * getCurrencyRate("EUR","USD");
 
 //Sending data to popup.js
@@ -81,9 +84,9 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
       total_price_eur: Math.round(total_price_eur),
 	  total_price_usd: Math.round(total_price_usd),
       initial_price_eur: Math.round(price_eur),
-      custom_value: Math.round(getCustomValue(price_eur)),
-	  excise_value: Math.round(getExciseValue(capacity, year)),
-	  tax: Math.round(getTaxValue(total_price_eur_without_tax))
+      custom_value: Math.round(custom_value),
+	  excise_value: Math.round(excise_value),
+	  tax: Math.round(tax_value)
     };
     response(domInfo);
   }
